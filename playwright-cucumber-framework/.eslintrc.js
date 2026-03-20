4b3cfb4f0bd1cb2294e6a4a4f0608e0272a117a3
SunilKumarGT <53367272+SunilKumarGT@users.noreply.github.com>
2026-03-20 20:28:03 +0530

    Add files via upload

---
+++ playwright-cucumber-framework/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // TypeScript
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+
+    // General
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'no-duplicate-imports': 'error',
+    'sort-imports': ['warn', { ignoreDeclarationSort: true }],
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'reports/',
+    '*.js',
+    '!.eslintrc.js',
+  ],
+};
